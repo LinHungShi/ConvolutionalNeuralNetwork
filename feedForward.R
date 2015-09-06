a9d9a7fcc765297e2f1702056deb6e5f35f86330
LinHungShi <u9600114@gmail.com>
2015-09-07 00:04:37 +0800

    create function: updateWeight updateGradient updateWgradient updateWeight_

--- feedForward.R
+++ feedForward.R
@@ -1,48 +1,38 @@
-feedForward <- function(cnn, x, x_dim, batch){
+feedForward <- function(cnn){
     
     layers <- cnn$layer
- 
     layer_name <- cnn$layer_name
-    input <- x
-    input_dim <- x_dim
-    is.convfinish <- FALSE
+    input <- cnn$x
+    input_dim <- cnn$x_dim
     pre_numfp <- 1
+    simplify <- TRUE
+    
     for(i in 1:length(layers)){
-        if(toupper(layer_name[[i]]) %in% c('CONV','MP')){
+        
+        if(layer_name[[i]] %in% c('CONV','MP')){
             
-            if(toupper(layer_name[i]) == 'CONV')
+            layers[[i]] <- updateValue(layers[[i]], input, input_dim, batch, pre_numfp, simplify)
+            stride <- layers[[i]]$stride
+            
+            if(layer_name[i] == 'CONV')
                 window_dim <- layers[[i]]$kernel_dim
             else 
                 window_dim <- layers[[i]]$neighbor_dim
             
-             
-            stride <- layers[[i]]$stride
-            kernel_form <- transKerForm(input, input_dim, stride, window_dim, batch, pre_numfp)
-            
-            input_trans <- kernel_form$value
-            
-            #if(i == 3)browser()
-            value <- updateValue(layers[[i]], input_trans)
-          
-            cnn$layer[[i]]$value <- input
-            input <- value
             input_dim <- getNewInpDim(input_dim, window_dim, stride)
-            cnn$v_kerform[[i]] <- input_trans
-                
+            input <- layers[[i]]$output
+            
         }else{
             
-            if(is.convfinish == FALSE){
-                input <- matrix(input, nrow = batch, byrow = TRUE)
-                is.convfinish = TRUE
-            }
-            
-            value <- updateValue(layers[[i]], input)
-            cnn$layer[[i]]$value <- value
-            input <- value
+            layers[[i]] <- updateValue(layers[[i]], input)  
+            input <- layers[[i]]$output
             
         }
-        pre_numfp <- layers[[i]]$num_fp
        
     }
+    
+    output <- input
+    cnn$layers <- layers
+    cnn$output <- output
     return (cnn)
 }
